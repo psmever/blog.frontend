59f2ea42d9ed79b39c7d122300dc9519928a2374
psmever <psmever@gmail.com>
2020-06-06 19:35:39 +0900

    test.

--- src/pages/blog/MainPage.tsx
+++ src/pages/blog/MainPage.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 export default function MainPage() {
+
+
+    useEffect(() => {
+        console.debug(1);
+    });
+
+
+
     return (
         <div>
             MainPage
