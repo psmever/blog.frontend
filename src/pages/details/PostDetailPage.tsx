c2d28b44a1ad66a7b57d579e8710fcaf61cfb8be
psmever <psmever@gmail.com>
2020-06-07 15:52:48 +0900

    base publishing.

--- src/pages/details/PostDetailPage.tsx
+++ src/pages/details/PostDetailPage.tsx
@@ -1,57 +1,9 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-export default function PostPage() {
+export default function PostDetailPage() {
     return (
         <>
-            <header className="header text-center">
-                <h1 className="blog-name pt-lg-4 mb-0"><Link to="/">Anthony's Blog</Link></h1>
-
-                <nav className="navbar navbar-expand-lg navbar-dark" >
-
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div id="navigation" className="collapse navbar-collapse flex-column">
-                        <div className="profile-section pt-3 pt-lg-0">
-                            <img className="profile-image mb-3 rounded-circle mx-auto" src="/assets/images/profile.png" alt="" />
-
-                            <div className="bio mb-3">Hi, my name is Anthony Doe. Briefly introduce yourself here. You can also provide a link to the about page.<br /><Link to="/">Find out more about me</Link></div>
-                            {/* <!--//bio--> */}
-                            <ul className="social-list list-inline py-3 mx-auto">
-                                <li className="list-inline-item"><Link to="/"><i className="fab fa-twitter fa-fw"></i></Link></li>
-                                <li className="list-inline-item"><Link to="/"><i className="fab fa-linkedin-in fa-fw"></i></Link></li>
-                                <li className="list-inline-item"><Link to="/"><i className="fab fa-github-alt fa-fw"></i></Link></li>
-                                <li className="list-inline-item"><Link to="/"><i className="fab fa-stack-overflow fa-fw"></i></Link></li>
-                                <li className="list-inline-item"><Link to="/"><i className="fab fa-codepen fa-fw"></i></Link></li>
-                            </ul>
-                            {/* <!--//social-list--> */}
-                            <hr />
-                        </div>
-                        {/* <!--//profile-section--> */}
-
-                        <ul className="navbar-nav flex-column text-left">
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link"><i className="fas fa-home fa-fw mr-2"></i>Blog Home <span className="sr-only">(current)</span></Link>
-                            </li>
-                            <li className="nav-item active">
-                                <Link to="/" className="nav-link"><i className="fas fa-bookmark fa-fw mr-2"></i>Blog Post</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link"><i className="fas fa-user fa-fw mr-2"></i>About Me</Link>
-                            </li>
-                        </ul>
-
-                        <div className="my-2 my-md-3">
-
-                            <Link to="/" className="btn btn-primary">Get in Touch</Link>
-
-                        </div>
-                    </div>
-                </nav>
-            </header>
-
             <div className="main-wrapper">
 
                 <article className="blog-post px-3 py-5 p-md-5">
@@ -73,7 +25,7 @@ export default function PostPage() {
                             <pre>
                                 <code>
 
-        code contents
+                code contents
 
                                 </code>
                             </pre>
@@ -205,14 +157,6 @@ export default function PostPage() {
                 </section>
                 {/* <!--//promo-section--> */}
 
-                <footer className="footer text-center py-2 theme-bg-dark">
-
-                    {/* <!-- */}
-                        {/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :)  */}
-                    {/* --> */}
-                    <small className="copyright">Designed with <i className="fas fa-heart" style={{color: "#fb866a"}}></i> by <Link to="/">Xiaoying Riley</Link> for developers</small>
-
-                </footer>
 
             </div>
             {/* <!--//main-wrapper--> */}
