930d3e713bfd1ab7ddab8d6c16cb880a1dc24215
psmever <psmever@gmail.com>
2020-06-07 16:09:57 +0900

    코드정링.

--- src/modules/Routes.tsx
+++ src/modules/Routes.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Router, Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
 
-import * as Pages from "components/pages";
-
 import MainLayoutComponents from 'components/MainLayoutComponents'
+import * as Pages from "components/pages";
 
 interface RootProps  {
     Routerhistory: any
