2256ddd3b6415116f19ee5d34a221c96a6c05d2e
psmever <psmever@gmail.com>
2020-06-07 15:58:42 +0900

    Component 위치 수정.

--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as DefaultPage } from './DefaultPage';
-
-export { default as MainPage } from './details/MainPage';
-export { default as AboutPage } from './details/AboutPage';
-export { default as PostDetailPage } from './details/PostDetailPage';
