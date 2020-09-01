5b494d0d5cdb1e0f44700b7ca1d443d093af797b
psmever <psmever@gmail.com>
2020-09-01 14:00:24 +0900

    login, token test working.

--- src/modules/API.ts
+++ src/modules/API.ts
@@ -1,14 +1,18 @@
-import * as GlobalService from 'lib/DefaultAxios';
+import * as DefaultAxios from 'lib/DefaultAxios';
 
 
 export function checkServer ():Promise<any> {
-    return GlobalService._Axios_.get('/api/system/check-notice');
+    return DefaultAxios._Axios_.get('/api/system/check-notice');
 };
 
 
 export function login ():Promise<any> {
-    return GlobalService._Axios_.post('/api/v1/auth/login', {
+    return DefaultAxios._Axios_.post('/api/v1/auth/login', {
         "email": "psmever@gmail.com",
         "password": "alsrns78"
     });
+};
+
+export function loginCheck ():Promise<any> {
+    return DefaultAxios._Axios_.post('/api/v1/auth/login-check');
 };
