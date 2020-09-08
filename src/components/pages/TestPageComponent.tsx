d38ca339ed2f3615a57bc0843e016a3d362cb026
psmever <psmever@gmail.com>
2020-09-09 00:27:04 +0900

    [update] 테스트 코드 삭제.

--- src/components/pages/TestPageComponent.tsx
+++ src/components/pages/TestPageComponent.tsx
@@ -1,41 +1,11 @@
 import React, { useEffect } from 'react';
-import * as API from 'modules/API';
-import * as Helper from 'lib/Helper';
 
 export default function TestPage() {
 
-    // TODO 로그인 토큰 리프레시 테스트 하고 프로젝트 진행~
-    const loginRequest = async () => {
-        console.debug('::login start::');
-        const loginResult = await API.login();
-        Helper.saveLoginToken(loginResult.payload);
-        console.debug('::login end::');
-    }
 
-    const loginRequest2 = async () => {
-        console.debug('::testCheck start::');
-        const loginResult = await API.testCheck();
-        // console.debug(loginResult.status);
-        if(loginResult.status === false) {
-            alert(loginResult.message);
-        }
-        // Helper.saveLoginToken(loginResult.data);
-        console.debug('::testCheck end::');
-        // loginCheckRequest();
-    }
 
     useEffect(() => {
         console.debug(':: TestPage Start :: ');
-
-        // // Helper.removeLoginToken();
-        // console.debug(Helper.getAccessToken() ? 'login True' : 'login False');
-
-        if(Helper.isEmpty(Helper.getAccessToken()) === true) {
-            loginRequest();
-        } else {
-            loginRequest2();
-        }
-
     }, []);
 
     return (
