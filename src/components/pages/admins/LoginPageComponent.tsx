9e27c7336bcf036ca168d75a011792a92e849107
psmever <psmever@gmail.com>
2020-09-10 00:21:20 +0900

    [UPDATE] 로그인 페이지 처리.

--- src/components/pages/admins/LoginPageComponent.tsx
+++ src/components/pages/admins/LoginPageComponent.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React,{ KeyboardEvent, useRef } from 'react';
 import * as AdminLoginStyleComponent from "styles/AdminLogin";
 import useLogin from 'hooks/useLogin';
 
 export default function LoginPage() {
+
+    // focus 용 Ref.
+    const inputPasswordRef = useRef<HTMLInputElement | null>(null);
 
     const {
         inputEmail,
@@ -11,7 +14,26 @@ export default function LoginPage() {
         _handleInputEmailChange,
         _handleInputPasswordChange,
         _handleLoginButtonClick,
+        _handelLogin,
     } = useLogin();
+
+    // 엔터키 처리.
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.key !== 'Enter') return;
+
+        const target_id: string = (e.target as HTMLElement).id;
+
+        if(target_id === 'Email') {
+            console.debug('email');
+            inputPasswordRef.current?.focus();
+        }
+
+        if(target_id === 'Password'){
+            console.debug('password');
+            _handelLogin();
+        }
+    }
+
 
     return (
         <>
@@ -22,17 +44,22 @@ export default function LoginPage() {
                     <AdminLoginStyleComponent.LoginUserName
                         type="text"
                         name="u"
+                        id="Email"
                         value={inputEmail}
                         onChange={ e => _handleInputEmailChange(e.target.value) }
                         placeholder="Email..."
+                        onKeyPress={e => onEnter(e)}
                     />
 
                     <AdminLoginStyleComponent.LoginPassword
                         type="password"
                         name="p"
+                        id="Password"
                         value={inputPassword}
                         onChange={ e => _handleInputPasswordChange(e.target.value) }
                         placeholder="Password"
+                        onKeyPress={e => onEnter(e)}
+                        ref={inputPasswordRef}
                     />
 
                     <AdminLoginStyleComponent.LoginButton
