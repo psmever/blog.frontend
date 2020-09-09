b8c3b9a394fda1095bc6d0af40b2cab1a0864c45
psmever <psmever@gmail.com>
2020-09-10 00:21:20 +0900

    [UPDATE] 로그인 페이지 처리.

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loginRequestInterface } from 'commonTypes';
 import { attemptLoginAction } from 'modules/redux/authenticate';
 import { RootState } from 'modules';
+import * as Helper from 'lib/Helper';
+import history from 'modules/History';
 
 export default function useLogin() {
 
@@ -24,22 +26,37 @@ export default function useLogin() {
 
     // login button click  event
     const _handleLoginButtonClick = () => {
+        _handelLogin();
+    }
+
+    const _handelLogin = () => {
+        // TODO 실패시 어떻게 할껀지?
         const loginPayload : loginRequestInterface = {
             email: inputEmail,
             password: inputPassword,
         }
 
         dispatch(attemptLoginAction(loginPayload));
-        // TODO 실패시 어떻게 할껀지?
     }
 
     useEffect(() => {
         console.debug(':: useLogin loading ::');
     }, []);
 
+    // 로그인 스토어 변경 처리.
     useEffect(() => {
-        // TODO login redux state 구조 수정 해야함.
-        console.debug(login_state);
+        if(login_state.status === 'idle') { // 로그인 시도 전.
+
+        } else if(login_state.status === 'loading') { // 로그인 시도중.
+
+        } else if(login_state.status === 'success') { // 로그인 완료.
+            Helper.saveLoginToken(login_state.data); // 토큰 저장.
+
+            history.push(process.env.PUBLIC_URL + '/');
+
+        } else if(login_state.status === 'failure') { // 시도 에러.
+
+        }
     }, [login_state])
 
     return {
@@ -49,6 +66,7 @@ export default function useLogin() {
         _handleInputEmailChange,
         _handleInputPasswordChange,
         _handleLoginButtonClick,
+        _handelLogin,
 
     };
 }
