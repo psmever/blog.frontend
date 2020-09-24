6b3dbb328d516dbb8eae1a0302d932cb2a965720
psmever <psmever@gmail.com>
2020-09-24 14:14:16 +0900

    [UPDATE] Redux log 옵션 추가.

--- src/modules/configureStore.ts
+++ src/modules/configureStore.ts
@@ -4,27 +4,28 @@ import createSagaMiddleware from 'redux-saga'
 import { routerMiddleware } from 'connected-react-router'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { History } from 'history'
-
+import * as _ from "lodash";
 import { RootState, rootSaga, createRootReducer } from 'modules';
 
 export default function configureStore(history: History, initialState: RootState): Store<RootState> {
 
     let compose;
 
-    const isDevelopment = process.env.REACT_APP_ENV === 'production';
+    const isDevelopment = process.env.REACT_APP_ENV === 'production' ? false : true;
+    const useReduxLogger = !_.isUndefined(process.env.REACT_APP_USE_REDUX_LOGGER) && process.env.REACT_APP_USE_REDUX_LOGGER === 'TRUE' ? true : false;
 
     const composeEnhancers = composeWithDevTools({});
     const sagaMiddleware = createSagaMiddleware();
-    const useReduxLogger = false;
+
 
     if( isDevelopment ) {
-        compose = applyMiddleware(routerMiddleware(history), sagaMiddleware);
-    } else {
         if(useReduxLogger) {
             compose = composeEnhancers(applyMiddleware(routerMiddleware(history), sagaMiddleware, createLogger()));
         } else {
             compose = composeEnhancers(applyMiddleware(routerMiddleware(history), sagaMiddleware));
         }
+    } else {
+        compose = applyMiddleware(routerMiddleware(history), sagaMiddleware);
     }
 
     const store = createStore(
