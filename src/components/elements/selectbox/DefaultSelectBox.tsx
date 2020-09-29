b5893433de24a207de75d02b749c0b32d93a05fe
psmever <psmever@gmail.com>
2020-09-29 20:53:13 +0900

    [Working] write-selectbox.

--- src/components/elements/selectbox/DefaultSelectBox.tsx
+++ src/components/elements/selectbox/DefaultSelectBox.tsx
@@ -1,39 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Select from 'react-select';
 import { defaultSelectBoxInterface, defaultSelectBoxItems } from 'commonTypes';
 
 interface DefaultSelectBoxProps {
     options: defaultSelectBoxInterface,
-    selectChange : ( category: string ) => void;
-    selectItem : string,
+    selectOption : ( selectitem: defaultSelectBoxItems) => void;
+    selectValue : defaultSelectBoxItems;
 }
 
 export default function DefaultSelectBox({
     options,
-    selectChange,
-    selectItem,
+    selectOption,
+    selectValue,
 }: DefaultSelectBoxProps) {
 
-    const [selectedOption, setSelectedOption] = useState();
-
     const _handleChange = (e:any) => {
-        selectChange(e.value);
+        selectOption(e)
     }
 
     // TODO 2020-09-29 00:19  선택 에러 남.
-
-    useEffect(() => {
-        console.debug(selectItem);
-        // setSelectedOption({value: selectItem});
-        setSelectedOption({value: 'S05990', label: 'runners' });
-        // setSelectedOption({ value: "true", label: "Younger" });
-    }, [selectItem]);
-
     return (
         <div style={{width: '100%'}}>
             <Select
-                defaultValue={selectedOption}
-                value={selectedOption}
+                defaultValue={selectValue}
+                value={selectValue}
                 onChange={_handleChange}
                 options={options}
             />
