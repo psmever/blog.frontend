d33d9fabdbcda40208f36292920a5ff101366c6a
psmever <psmever@gmail.com>
2020-10-02 14:12:07 +0900

    [Working] 메인 페이지.

--- src/components/pages/details/PostDetailPageComponent.tsx
+++ src/components/pages/details/PostDetailPageComponent.tsx
@@ -2,8 +2,39 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import * as PostDetailStyleComponent from "styles/PostDetail";
 import * as StyledIcons from 'styles/StyledIcons';
+import useDetail from 'hooks/useDetail';
+import ReactMarkdown from 'react-markdown';
+import Markdown,{compiler} from 'markdown-to-jsx';
 
 export default function PostDetailPage() {
+
+    const MyParagraph = (props:any) => (
+        <p>This is inside MyParagraph, and the value is {props.children}</p>
+      );
+
+      // see https://github.com/rexxars/react-markdown#node-types
+      const renderers = {
+        paragraph: (props : any) => <MyParagraph {...props} />
+      };
+
+
+    const markdown = `
+Foo \`console.log('foo')\` baz
+
+| foo | bar |
+|-----|-----|
+| bar | baz |
+
+\`\`\`js
+const foo = "bar"
+\`\`\`
+`
+
+
+    const {
+        postBaseStateDetail
+    } = useDetail();
+
     return (
         <>
             <PostDetailStyleComponent.MainWrapper>
@@ -13,10 +44,10 @@ export default function PostDetailPage() {
                     <PostDetailStyleComponent.Container>
 
                         <PostDetailStyleComponent.Header>
-                            <PostDetailStyleComponent.HeaderTitle>Why Every Developer Should Have A Blog</PostDetailStyleComponent.HeaderTitle>
+                            <PostDetailStyleComponent.HeaderTitle>{postBaseStateDetail.data?.post_title}</PostDetailStyleComponent.HeaderTitle>
                             <PostDetailStyleComponent.HeaderMeta>
-                                <PostDetailStyleComponent.HeaderDate>Published 3 months ago</PostDetailStyleComponent.HeaderDate>
-                                <PostDetailStyleComponent.HeaderTime>5 min read</PostDetailStyleComponent.HeaderTime>
+                                <PostDetailStyleComponent.HeaderDate>{postBaseStateDetail.data?.detail_updated} </PostDetailStyleComponent.HeaderDate>
+                                <PostDetailStyleComponent.HeaderTime> 5 min read</PostDetailStyleComponent.HeaderTime>
                                 <PostDetailStyleComponent.HeaderComment>
                                     <PostDetailStyleComponent.HeaderCommentLink to="/">4 comments</PostDetailStyleComponent.HeaderCommentLink>
                                 </PostDetailStyleComponent.HeaderComment>
@@ -24,54 +55,19 @@ export default function PostDetailPage() {
                         </PostDetailStyleComponent.Header>
 
                         <PostDetailStyleComponent.PostBody>
-                            <PostDetailStyleComponent.Banner>
-                                <PostDetailStyleComponent.BannerLink to="/">
-                                    <PostDetailStyleComponent.BannerImage src="/assets/images/blog/blog-post-banner.jpg" alt="" />
-                                </PostDetailStyleComponent.BannerLink>
-                                <PostDetailStyleComponent.BannerImageCaption>
-                                    Image Credit:
-                                        <PostDetailStyleComponent.BannerImageCaptionLink to="/">
-                                            made4dev.com (Premium Programming T-shirts)
-                                        </PostDetailStyleComponent.BannerImageCaptionLink>
-                                </PostDetailStyleComponent.BannerImageCaption>
-                            </PostDetailStyleComponent.Banner>
-                            <PostDetailStyleComponent.PTag>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. </PostDetailStyleComponent.PTag>
+                            {/* <ReactMarkdown
+                                // source={postBaseStateDetail.data?.contents_text}
+                                source={markdown}
+                                renderers={renderers}
+                            /> */}
 
-                            <PostDetailStyleComponent.ContentTitle1>
-                                Code Block Example
-                            </PostDetailStyleComponent.ContentTitle1>
-                            <PostDetailStyleComponent.PTag>You can get more info at <Link to="/">https://highlightjs.org/</Link>. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. </PostDetailStyleComponent.PTag>
-                            <pre>
-                                <code>
+{/* {postBaseStateDetail.data?.contents_html} */}
+                        {/* <Markdown
+                            children={markdown}
+                        /> */}
 
-                code contents
-
-                                </code>
-                            </pre>
-                            <PostDetailStyleComponent.ContentTitle1>
-                                Typography
-                            </PostDetailStyleComponent.ContentTitle1>
-                            <PostDetailStyleComponent.PTag>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</PostDetailStyleComponent.PTag>
-                            <PostDetailStyleComponent.ContentTitle2>
-                                Bullet Points:
-                            </PostDetailStyleComponent.ContentTitle2>
-                            <PostDetailStyleComponent.ContentPoint1>
-                                <PostDetailStyleComponent.ContentPointItem>Lorem ipsum dolor sit amet consectetuer.</PostDetailStyleComponent.ContentPointItem>
-                                <PostDetailStyleComponent.ContentPointItem>Aenean commodo ligula eget dolor.</PostDetailStyleComponent.ContentPointItem>
-                                <PostDetailStyleComponent.ContentPointItem>Aenean massa cum sociis natoque penatibus.</PostDetailStyleComponent.ContentPointItem>
-                            </PostDetailStyleComponent.ContentPoint1>
-                            <PostDetailStyleComponent.ContentPoint2>
-                                <PostDetailStyleComponent.ContentPointItem>Lorem ipsum dolor sit amet consectetuer.</PostDetailStyleComponent.ContentPointItem>
-                                <PostDetailStyleComponent.ContentPointItem>Aenean commodo ligula eget dolor.</PostDetailStyleComponent.ContentPointItem>
-                                <PostDetailStyleComponent.ContentPointItem>Aenean massa cum sociis natoque penatibus.</PostDetailStyleComponent.ContentPointItem>
-                            </PostDetailStyleComponent.ContentPoint2>
-                            <PostDetailStyleComponent.ContentTitle2>
-                                Quote Example:
-                            </PostDetailStyleComponent.ContentTitle2>
-                            <PostDetailStyleComponent.BlockQuote>
-                                <PostDetailStyleComponent.BlockQuotePtag>You might not think that programmers are artists, but programming is an extremely creative profession. It's logic-based creativity.</PostDetailStyleComponent.BlockQuotePtag>
-                                <PostDetailStyleComponent.BlockQuoteFooter>John Romero</PostDetailStyleComponent.BlockQuoteFooter>
-                            </PostDetailStyleComponent.BlockQuote>
+{compiler(markdown)}
+{compiler(markdown, { disableParsingRawHTML: true })}
 
                         </PostDetailStyleComponent.PostBody>
 
