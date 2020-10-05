edddcaf00d58513930bddeb720ee62ea64bf3a9c
psmever <psmever@gmail.com>
2020-10-06 00:32:51 +0900

    [ADD] 상단, 하단 문구 및 링크 수정.

--- src/components/common/FooterComponent.tsx
+++ src/components/common/FooterComponent.tsx
@@ -4,6 +4,18 @@ import * as FooterStyleComponent from "styles/Footer";
 import * as StyledIcons from 'styles/StyledIcons';
 
 export default function FooterComponent() {
+
+    const handleClickFrontSouce = () => {
+        window.open('https://github.com/psmever/blog.front', '_blank', 'noopener,noreferrer');
+    }
+
+
+    const handleClickBackendSource = () => {
+        window.open('https://github.com/psmever/blog.backend', '_blank', 'noopener,noreferrer');
+    }
+
+
+
     return (
         <>
         <FooterStyleComponent.MainWarpper>
@@ -12,7 +24,7 @@ export default function FooterComponent() {
                     {/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */}
                 {/* --> */}
                 <FooterStyleComponent.Copyright>
-                    Designed with <StyledIcons.HeartIcon/> by <Link to="/">Xiaoying Riley</Link> for developers
+                    <StyledIcons.HeartIcon/> Made in <Link to="/">@psmever</Link> &nbsp; Blog Version: {process.env.REACT_APP_VERSION} &nbsp; <Link to='' onClick={handleClickFrontSouce}>Front-Source</Link> &nbsp; <Link to='' onClick={handleClickBackendSource}>Backend-Source</Link>
                 </FooterStyleComponent.Copyright>
             </FooterStyleComponent.Footer>
         </FooterStyleComponent.MainWarpper>
