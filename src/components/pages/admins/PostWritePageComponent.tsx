dc6e611eba25e8b58ec6a53d427b5501296ece56
psmever <psmever@gmail.com>
2020-10-07 00:29:14 +0900

    [Working] write page.

--- src/components/pages/admins/PostWritePageComponent.tsx
+++ src/components/pages/admins/PostWritePageComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { editorContentsInterface } from 'commonTypes';
 import useWrite from 'hooks/useWrite';
 import history from 'modules/History';
@@ -14,77 +14,130 @@ import {
     CategorySelectBox,
     WriteTagBox,
     WriteBody,
+    PublishButton,
+    StyledApp,
+    StyledEditor,
+    StyledEditorTextArea,
+    StyledPreviewBox,
+    StyledPreviewC3,
+    StyledPreviewTitle,
     ButtonContainer,
-    ButtonBox,
-    PublishButton
 } from "styles/PostWriter";
-
 import { ButtonLoading } from 'components/elements';
-
-import hljs from 'highlight.js'
-import javascript from 'highlight.js/lib/languages/javascript'
-import 'highlight.js/styles/atom-one-light.css';
-
-/** MarkDown Editor Start */
-import MarkdownIt from 'markdown-it'
-import MdEditor from 'react-markdown-editor-lite'
-import 'react-markdown-editor-lite/lib/index.css';
-
+import { MarkdownRender } from 'components/elements';
 // https://stackblitz.com/edit/react-tag-input-1nelrc?file=index.js
 import { WithContext as ReactTags } from 'react-tag-input';
 import './ReactTagStyle.css';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
 
-hljs.registerLanguage('javascript', javascript)
+const mardownText = `
+# Blog.Backend
 
-// const mdParser = new MarkdownIt(/* Markdown-it options */);
-const mdParser = new MarkdownIt({
-    html:         false,        // Enable HTML tags in source
-    xhtmlOut:     false,        // Use '/' to close single tags (<br />).
-                            // This is only for full CommonMark compatibility.
-    breaks:       false,        // Convert '\n' in paragraphs into <br>
-    langPrefix:   'language-',  // CSS language prefix for fenced blocks. Can be
-                            // useful for external highlighters.
-    linkify:      false,        // Autoconvert URL-like text to links
+### Git First Push.
+Use the package manager [composer](https://getcomposer.org/) to install foobar.
+
+## Git Clone.
+
+\`\`\`bash
+git clone https://github.com/psmever/blog.backend.git blog.backend
+\`\`\`
+
+## Git Clone (Single Branch).
+
+\`\`\`bash
+git clone -b develop --single-branch https://github.com/psmever/blog.backend.git
+\`\`\`
+
+## Composer.
+\`\`\`bash
+composer install
+
+\`\`\`
+
+## First Config.
+\`\`\`bash
+composer install
+cp .env.example .env
+\`\`\`
+
+## Local Develop Server.
+\`\`\`bash
+php artisan serve
+\`\`\`
+
+## Browser.
+\`\`\`bash
+http://127.0.0.1:8000 || http://localhost:8000/
+\`\`\`
+
+## Ex Site..
+\`\`\`bash
+repository-pattern
+https://medium.com/dev-genius/laravel-api-repository-pattern-make-your-code-more-structured-the-simple-guide-5b770da766d7
+
+deploy
+https://jeromejaglale.com/doc/php/laravel_github_webhook
+
+deploy - envoy
+https://github.com/appkr/envoy
 
 
-    // Enable some language-neutral replacement + quotes beautification
-    typographer:  false,
+Rest-api-Response-Format
+https://github.com/cryptlex/rest-api-response-format
 
-    // Double + single quotes replacement pairs, when typographer enabled,
-    // and smartquotes on. Could be either a String or an Array.
-    //
-    // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
-    // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
-    quotes: '“”‘’',
-    highlight: function (str, lang) {
-        if (lang && hljs.getLanguage(lang)) {
-            try {
-                return hljs.highlight(lang, str).value;
-            } catch (__) {}
-            }
+\`\`\`
 
-            return ''; // use external default escaping
-        }
-});
+## CustomException
+\`\`\`
+throw new \App\Exceptions\CustomException('Something Went Wrong.');
 
-const KeyCodes = {
-    comma: 188,
-    enter: 13,
-};
+\`\`\`
 
-const delimiters = [KeyCodes.comma, KeyCodes.enter];
+## App Clear Script
+\`\`\`
 
-// 에디터 설정.
-const markdownEditorConfig = {
-    view: {
-        menu: false,
-        md: true,
-        html: true
-    }
-}
-/** MarkDown Editor End */
+composer app:clear
+composer test:clear
+\`\`\`
+
+## Server Deploy
+\`\`\`
+> composer global require laravel/envoy
+
+envoy run deploy_prod
+envoy run deploy_stage
+
+\`\`\`
+
+## etc message
+github actions test
+\`\`\`
+
+SlackMessage notifications
+https://medium.com/@olayinka.omole/sending-slack-notifications-from-your-laravel-app-1bdb6e4e4127
+https://www.lesstif.com/php-and-laravel/sending-slack-notifications-from-laravel-36209045.html
+\`\`\`
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+`;
 
 export default function WritePage() {
+
+    const KeyCodes = {
+        comma: 188,
+        enter: 13,
+    };
+
+    const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
+    const [markdown, setMarkdown] = useState(mardownText);
 
     const {
         editorTitle,
@@ -172,78 +225,46 @@ export default function WritePage() {
     // }
 
     // FIXME 2020-10-01 22:21  높이 스크롤 생기는 버그 수정.필요.
+    // https://codesandbox.io/s/nwm83w9y1l?file=/src/styles.css:0-91
     return (
         <>
             <MainWrapper>
                 <BlogWrite>
                     <Container>
-                        <Header>
-                            {/* <HeaderTitle>글등록.</HeaderTitle> */}
-                        </Header>
-
-                        <CategorySelectBox>
-                            <DefaultSelectBox
-                                options={ categoryThumbList }
-                                selectValue={editorCategoryThumb}
-                                selectOption={ _handleEditorCategorySelectItem }
-                            />
-                        </CategorySelectBox>
-
-                        <WriteTitleBox>
-                            <WriteTitleLabel htmlFor="writeTitle"></WriteTitleLabel>
-                            <WriteTitle type="text" id="writeTitle" placeholder="제목을 입력해 주세요."
-                                value={editorTitle}
-                                onChange={ e => setEditorTitle(e.target.value) }
-                            />
-                        </WriteTitleBox>
-
-                        <WriteTagBox>
-                            <ReactTags tags={ editorTagContents }
-                                suggestions={editorTagSuggestions}
-                                handleDelete={handleTagDelete}
-                                handleAddition={handleTagAddition}
-                                handleDrag={handleTagDrag}
-                                delimiters={delimiters}
-                                handleTagClick={handleTagClick}
-                                placeholder={'테그를 입력해 주세요'}
-                            />
-                        </WriteTagBox>
                         <WriteBody>
-                            <MdEditor
-                                plugins={[
-                                    'header',
-                                    // 'font-bold',
-                                    // 'font-italic',
-                                    // 'font-underline',
-                                    // 'font-strikethrough',
-                                    // 'list-unordered',
-                                    // 'list-ordered',
-                                    // 'block-quote',
-                                    // 'block-wrap',
-                                    // 'block-code-inline',
-                                    // 'block-code-block',
-                                    // 'table',
-                                    // 'image',
-                                    // 'link',
-                                    // 'clear',
-                                    // 'logger',
-                                    // 'mode-toggle',
-                                    // 'full-screen'
-                                ]}
-                                value = { editorContents.text }
-                                style = {{ height: "100%", width: "100%" }}
-                                renderHTML = {(text) => mdParser.render(editorTitle + text)}
-                                // renderHTML={text => <ReactMarkdown source={editorTitle + text} renderers={{ code: CodeBlock }}/>}
-                                onChange = {handleEditorContentsChange}
-                                placeholder = {"내용을 입력해 주세요."}
-                                config = {markdownEditorConfig}
-                            />
+                            <StyledApp>
+                                <StyledEditor>
+                                    <WriteTitleLabel htmlFor="writeTitle"></WriteTitleLabel>
+                                    <WriteTitle type="text" id="writeTitle" placeholder="제목을 입력해 주세요." value={editorTitle} onChange={ e => setEditorTitle(e.target.value) } />
+                                    <ReactTags tags={ editorTagContents } suggestions={editorTagSuggestions} handleDelete={handleTagDelete} handleAddition={handleTagAddition} handleDrag={handleTagDrag} delimiters={delimiters} handleTagClick={handleTagClick} placeholder={'테그를 입력해 주세요'}/>
+                                    <StyledEditorTextArea onChange={e => setMarkdown(e.target.value)} value={markdown} />
+
+
+<div className="sc-kafWEX eFDVNK">
+    <div className="sc-lhVmIH iWtevg"><button className="sc-jtRfpW kZfGaX"><svg stroke="currentColor" fill="currentColor"
+                stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+            </svg><span>나가기</span></button>
+        <div className="sc-bYSBpT jTukdO"><button color="lightGray"
+                className="sc-dnqmqq fzKLng sc-elJkPf cDxJYR">임시저장</button><button color="teal"
+                className="sc-dnqmqq gzELJz sc-elJkPf cDxJYR">출간하기</button></div>
+    </div>
+</div>
+                                </StyledEditor>
+
+                                <StyledPreviewBox>
+                                    <StyledPreviewTitle>하이</StyledPreviewTitle>
+                                    <StyledPreviewC3>
+                                        <MarkdownRender
+                                            markdownText={mardownText}
+                                        />
+                                    </StyledPreviewC3>
+                                </StyledPreviewBox>
+
+                            </StyledApp>
+
                         </WriteBody>
-                        <ButtonContainer>
-                            <ButtonBox buttonType={"Home"} onClick={handleHomeButtonClick}><PublishButton>홈</PublishButton></ButtonBox>
-                            { post_create_state.status === 'loading' ? <ButtonBox buttonType={"Save"}><ButtonLoading/></ButtonBox> : <ButtonBox buttonType={"Save"} onClick={handleSaveButtonClick}><PublishButton>저장</PublishButton></ButtonBox> }
-                            { post_publish_state.status === 'loading' ? <ButtonBox buttonType={"Publish"}><ButtonLoading/></ButtonBox> : <ButtonBox buttonType={"Publish"} onClick={handlePublishButtonClick}><PublishButton>게시</PublishButton></ButtonBox> }
-                        </ButtonContainer>
+
                     </Container>
                     {/* <!--//container--> */}
                 </BlogWrite>
