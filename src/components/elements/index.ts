c66756347d274c1b177e40c2aa2d0db21bf48f50
psmever <psmever@gmail.com>
2020-10-16 00:35:37 +0900

    [UPDATE] infinite scroll.

--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -2,6 +2,7 @@ export { default as DefaultLoading } from './loading/DefaultLoading';
 export { default as LoginButtonLoading } from './loading/LoginButtonLoading';
 export { default as BodyLoading } from './loading/BodyLoading';
 export { default as ButtonLoading } from './loading/ButtonLoading';
+export { default as PageLoginButtonLoading } from './loading/PageLoginButtonLoading';
 export { default as MarkdownRender } from './markdown/MarkdownRender';
 
 export { default as DefaultButton } from './button/DefaultButton';
