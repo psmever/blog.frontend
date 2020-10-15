746b9dfef10140c3bd50b3cb93c35e7723eb26e5
psmever <psmever@gmail.com>
2020-10-15 15:56:52 +0900

    [UPDATE] api 모듈 변경.

--- src/components/pages/admins/LogoutPageComponent.tsx
+++ src/components/pages/admins/LogoutPageComponent.tsx
@@ -1,33 +1,33 @@
 import React, {useEffect} from 'react';
 import { BodyLoading } from 'components/elements';
-import _Alert_ from 'lib/_Alert_';
 import * as Helper from 'lib/Helper';
 import {logout}  from 'modules/API';
 import history from 'modules/History';
+import { useToasts } from 'react-toast-notifications'
 
 export default function LogoutPage() {
-
-
+    const { addToast } = useToasts();
 
     useEffect(() => {
         async function callLogout() {
             const response = await logout();
             if(response.status === true) {
                 Helper.removeLoginToken();
-                _Alert_.thenGoHome({text:'로그아웃 되었습니다.'});
+                addToast('로그아웃 되었습니다.', { appearance: 'success', autoDismiss: true });
+                history.push(process.env.PUBLIC_URL + '/');
             } else {
                 history.push(process.env.PUBLIC_URL + '/');
             }
         }
 
         const checkResult = Helper.getLocalToken();
-        console.debug('logout login_state: ', checkResult.login_state);
         if(checkResult.login_state === true) {
             callLogout();
         } else {
-            _Alert_.thenGoHome({text:'로그인 되어 있지 않습니다.'});
+            addToast('로그인 상태가 아닙니다.', { appearance: 'info', autoDismiss: true });
+            history.push(process.env.PUBLIC_URL + '/');
         }
-    }, []);
+    }, [addToast]);
 
     return (
         <>
--- src/hooks/useMain.ts
+++ src/hooks/useMain.ts
@@ -1,27 +1,21 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'modules';
-import * as Helper from 'lib/Helper';
 import {
     postGetListAction
 } from 'modules/redux/post';
+
 
 export default function useMain() {
     const dispatch = useDispatch();
     const postBaseStateLists = useSelector((state: RootState) => state.post.lists);
 
-
-
     useEffect(() => {
         dispatch(postGetListAction(1));
-
         // FIXME 2020-10-05 01:13 경고 disable 수정 필요.
         // eslint-disable-next-line
     }, [])
 
-    useEffect(() => {
-        console.debug(Helper.getLocalToken());
-    }, [])
     return {
         postBaseStateLists
     }
