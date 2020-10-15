d68bacf2450697af9b0d4d61d229733e522011fe
psmever <psmever@gmail.com>
2020-10-15 18:27:44 +0900

    [Working] posts list.

--- src/hooks/useMain.ts
+++ src/hooks/useMain.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'modules';
 import {
@@ -8,15 +8,37 @@ import {
 
 export default function useMain() {
     const dispatch = useDispatch();
-    const postBaseStateLists = useSelector((state: RootState) => state.post.lists);
+    const [ postItemList, setPostItemList] = useState('');
+    const [ postListPageNumber, setPostListPageNumber] = useState<number>(1);
+    const basePostListsState = useSelector((state: RootState) => state.post.lists);
+
+
+    const fetchMoreData = () => {
+        // a fake async api call like which sends
+        // 20 more records in 1.5 secs
+        setTimeout(() => {
+            setPostListPageNumber(postListPageNumber+1)
+            // dispatch(postGetListAction(postListPageNumber+1));
+        }, 1500);
+    };
 
     useEffect(() => {
         dispatch(postGetListAction(1));
         // FIXME 2020-10-05 01:13 경고 disable 수정 필요.
         // eslint-disable-next-line
-    }, [])
+    }, []);
 
+    useEffect(() => {
+        dispatch(postGetListAction(postListPageNumber));
+    }, [postListPageNumber]);
+
+    useEffect(() => {
+        // console.debug('status', basePostListsState.status);
+        // console.debug('posts', basePostListsState.posts);
+        // console.debug('length', basePostListsState.posts.length);
+    }, [basePostListsState]);
     return {
-        postBaseStateLists
+        basePostListsState,
+        fetchMoreData
     }
 }
