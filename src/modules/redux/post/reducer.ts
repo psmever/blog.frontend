f22daf5275c4710df591443a851d3b0378f7b338
psmever <psmever@gmail.com>
2020-10-16 14:06:50 +0900

    [UPDATE] Redux-Saga 정리.

--- src/modules/redux/post/reducer.ts
+++ src/modules/redux/post/reducer.ts
@@ -2,178 +2,21 @@ import { createReducer } from "typesafe-actions";
 import {
     axiosReturnInterface,
     postSagaState,
-    apiPostCreateResultInterface,
-    apiPostEditResultInterface,
-    apiPostDetailResultInterface,
-    apiResultErrorInterface,
     apiPostListResultInterface,
 } from 'commonTypes';
-import { SagaTypes, SagaAction } from 'modules/reduxActiontTypes';
+import { SagaTypes } from 'modules/reduxActiontTypes';
 
 const initialState: postSagaState = {
-    create: {
-        status: 'idle',
-    },
-    edit: {
-        status: 'idle',
-    },
-    publish: {
-        status: 'idle',
-    },
-    update: {
-        status: 'idle',
-    },
     lists: {
         status: 'idle',
         per_page: 0,
         current_page: 0,
         hasmore: true,
         posts: [],
-    },
-    detail: {
-        status: 'idle',
     }
 }
 
 export const postagaReducer = createReducer<postSagaState>(initialState, {
-    [SagaTypes.POST_CREATE_REQUEST_START]: (state: postSagaState) => {
-        return {
-            ...state,
-            create: {
-                status: 'loading',
-            }
-        }
-    },
-    [SagaTypes.POST_CREATE_REQUEST_SUCCESS]: (state: postSagaState, action: axiosReturnInterface<apiPostCreateResultInterface>) => {
-        return {
-            ...state,
-            create: {
-                status: 'success',
-                data: action.payload,
-            }
-        };
-    },
-    [SagaTypes.POST_CREATE_REQUEST_ERROR]: (state: postSagaState, action: axiosReturnInterface<apiResultErrorInterface>) => {
-        return {
-            ...state,
-            create: {
-                status: 'failure',
-                message: action.payload.message,
-            }
-        };
-    },
-    [SagaTypes.POST_CREATE_REQUEST_RESET]: (state: postSagaState) => {
-        return {
-            ...state,
-            create: {
-                status: 'idle',
-            }
-        };
-    },
-
-    [SagaTypes.POST_EDIT_REQUEST_START]: (state: postSagaState) => {
-        return {
-            ...state,
-            edit: {
-                status: 'loading',
-            }
-        }
-    },
-    [SagaTypes.POST_EDIT_REQUEST_SUCCESS]: (state: postSagaState, action: SagaAction<apiPostEditResultInterface>) => {
-        return {
-            ...state,
-            edit: {
-                status: 'success',
-                data: action.payload,
-            }
-        };
-    },
-    [SagaTypes.POST_EDIT_REQUEST_ERROR]: (state: postSagaState, action: axiosReturnInterface<apiPostEditResultInterface>) => {
-        return {
-            ...state,
-            edit: {
-                status: 'failure',
-                message: action.message,
-            }
-        };
-    },
-    [SagaTypes.POST_EDIT_REQUEST_RESET]: (state: postSagaState) => {
-        return {
-            ...state,
-            edit: {
-                status: 'idle',
-            }
-        };
-    },
-
-    [SagaTypes.POST_PUBLISH_REQUEST_START]: (state: postSagaState) => {
-        return {
-            ...state,
-            publish: {
-                status: 'loading',
-            }
-        }
-    },
-    [SagaTypes.POST_PUBLISH_REQUEST_SUCCESS]: (state: postSagaState, action: SagaAction<axiosReturnInterface<any>>) => {
-        return {
-            ...state,
-            publish: {
-                status: 'success',
-            }
-        };
-    },
-    [SagaTypes.POST_PUBLISH_REQUEST_ERROR]: (state: postSagaState, action: axiosReturnInterface<any>) => {
-        return {
-            ...state,
-            publish: {
-                status: 'failure',
-                message: action.message,
-            }
-        };
-    },
-    [SagaTypes.POST_PUBLISH_REQUEST_RESET]: (state: postSagaState) => {
-        return {
-            ...state,
-            publish: {
-                status: 'idle',
-            }
-        };
-    },
-
-    [SagaTypes.POST_UPDATE_REQUEST_START]: (state: postSagaState) => {
-        return {
-            ...state,
-            update: {
-                status: 'loading',
-            }
-        }
-    },
-    [SagaTypes.POST_UPDATE_REQUEST_SUCCESS]: (state: postSagaState, action: axiosReturnInterface<any>) => {
-        return {
-            ...state,
-            update: {
-                status: 'success',
-            }
-        };
-    },
-    [SagaTypes.POST_UPDATE_REQUEST_ERROR]: (state: postSagaState, action: axiosReturnInterface<any>) => {
-        return {
-            ...state,
-            update: {
-                status: 'failure',
-                message: action.message,
-            }
-        };
-    },
-    [SagaTypes.POST_UPDATE_REQUEST_RESET]: (state: postSagaState) => {
-        return {
-            ...state,
-            update: {
-                status: 'idle',
-            }
-        };
-    },
-
     [SagaTypes.POST_LIST_REQUEST_START]: (state: postSagaState) => {
         return {
             ...state,
@@ -219,41 +62,6 @@ export const postagaReducer = createReducer<postSagaState>(initialState, {
                 current_page: state.lists.current_page,
                 hasmore: false,
                 posts: [],
-            }
-        };
-    },
-
-    [SagaTypes.POST_DETAIL_REQUEST_START]: (state: postSagaState) => {
-        return {
-            ...state,
-            detail: {
-                status: 'loading',
-            }
-        }
-    },
-    [SagaTypes.POST_DETAIL_REQUEST_SUCCESS]: (state: postSagaState, action: axiosReturnInterface<apiPostDetailResultInterface>) => {
-        return {
-            ...state,
-            detail: {
-                status: 'success',
-                data: action.payload
-            }
-        };
-    },
-    [SagaTypes.POST_DETAIL_REQUEST_ERROR]: (state: postSagaState, action: axiosReturnInterface<apiPostDetailResultInterface>) => {
-        return {
-            ...state,
-            detail: {
-                status: 'failure',
-                message: action.message,
-            }
-        };
-    },
-    [SagaTypes.POST_DETAIL_REQUEST_RESET]: (state: postSagaState) => {
-        return {
-            ...state,
-            detail: {
-                status: 'idle',
             }
         };
     },
--- src/modules/redux/post/sagas.ts
+++ src/modules/redux/post/sagas.ts
@@ -1,55 +1,10 @@
 import { takeLatest, fork, call, put } from "redux-saga/effects";
 import { SagaTypes } from 'modules/reduxActiontTypes';
-import { postRequestInterface } from 'commonTypes';
-import { postCreate, postEdit, postPublish, postUpdate, getPostList, getPostDetail } from 'modules/API';
-
-
-// 글등록 Saga
-function* postCreateActionSaga({payload} : {payload: postRequestInterface}) {
-    const response = yield call(postCreate, payload);
-    if(response.status === true) {
-        yield put({ type: SagaTypes.POST_CREATE_REQUEST_SUCCESS, payload: response.payload});
-    } else {
-        yield put({ type: SagaTypes.POST_CREATE_REQUEST_ERROR, payload: response});
-    }
-}
-
-// 글 수정용 정보 가지고 오기 Saga
-function* postEditActionSaga({post_uuid} : {post_uuid: string}) {
-    const response = yield call(postEdit, post_uuid);
-    if(response.status === true) {
-        yield put({ type: SagaTypes.POST_EDIT_REQUEST_SUCCESS, payload: response.payload});
-    } else {
-        yield put({ type: SagaTypes.POST_EDIT_REQUEST_ERROR, payload: response});
-    }
-}
-
-// 글 수정용 정보 가지고 오기 Saga
-function* postPublishActionSaga({post_uuid} : {post_uuid: string}) {
-    const response = yield call(postPublish, post_uuid);
-    if(response.status === true) {
-        yield put({ type: SagaTypes.POST_PUBLISH_REQUEST_SUCCESS, payload: response.payload});
-    } else {
-        yield put({ type: SagaTypes.POST_PUBLISH_REQUEST_ERROR, payload: response});
-    }
-}
-
-// 글내용 업데이트
-function* postUpdateActionSaga({post_uuid , payload} : {post_uuid: string, payload: postRequestInterface}) {
-    const response = yield call(postUpdate, {post_uuid, payload});
-    if(response.status === true) {
-        yield put({ type: SagaTypes.POST_UPDATE_REQUEST_SUCCESS, payload: response.payload});
-    } else {
-        yield put({ type: SagaTypes.POST_UPDATE_REQUEST_ERROR, payload: response});
-    }
-}
+import { getPostList } from 'modules/API';
 
 // 글 리스트 가지고 오기
 function* postGetListActionSaga( { pageNumber } : {pageNumber: number} ) {
     const response = yield call(getPostList, {pageNumber});
-    // console.debug(response);
-    // console.debug(response.payload);
-    // console.debug(response.payload.length);
     if(response.status === true && response.payload) {
         yield put({ type: SagaTypes.POST_LIST_REQUEST_SUCCESS, payload: response.payload});
     } else {
@@ -57,23 +12,8 @@ function* postGetListActionSaga( { pageNumber } : {pageNumber: number} ) {
     }
 }
 
-// 글 상세 정보 가지고 오기
-function* postDetailActionSaga( { slugTitle } : {slugTitle: string} ) {
-    const response = yield call(getPostDetail, {slugTitle});
-    if(response.status === true) {
-        yield put({ type: SagaTypes.POST_DETAIL_REQUEST_SUCCESS, payload: response.payload});
-    } else {
-        yield put({ type: SagaTypes.POST_DETAIL_REQUEST_ERROR, payload: response});
-    }
-}
-
 function* onBaseSagaWatcher() {
-    yield takeLatest(SagaTypes.POST_CREATE_REQUEST_START as any, postCreateActionSaga);
-    yield takeLatest(SagaTypes.POST_EDIT_REQUEST_START as any, postEditActionSaga);
-    yield takeLatest(SagaTypes.POST_PUBLISH_REQUEST_START as any, postPublishActionSaga);
-    yield takeLatest(SagaTypes.POST_UPDATE_REQUEST_START as any, postUpdateActionSaga);
     yield takeLatest(SagaTypes.POST_LIST_REQUEST_START as any, postGetListActionSaga);
-    yield takeLatest(SagaTypes.POST_DETAIL_REQUEST_START as any, postDetailActionSaga);
 }
 
 export default [
