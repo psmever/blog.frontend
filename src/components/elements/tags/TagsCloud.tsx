3488e9c248dfba982c2c28e2dea0e07c3999207b
psmever <psmever@gmail.com>
2020-10-18 11:35:11 +0900

    [Working] Tags.

--- src/components/elements/tags/TagsCloud.tsx
+++ src/components/elements/tags/TagsCloud.tsx
@@ -1,36 +1,24 @@
 import React from 'react'
-import { TagCloud } from 'react-tagcloud'
+import { TagCloud } from 'react-tagcloud';
+import {apiTagGoupListInterface} from 'commonTypes';
 
-const data = [
-  { value: 'jQuery', count: 25 },
-  { value: 'MongoDB', count: 18 },
-  { value: 'JavaScript', count: 38 },
-  { value: 'React', count: 30 },
-  { value: 'Nodejs', count: 28 },
-  { value: 'Express.js', count: 25 },
-  { value: 'HTML5', count: 33 },
-  { value: 'CSS3', count: 20 },
-  { value: 'Webpack', count: 22 },
-  { value: 'Babel.js', count: 7 },
-  { value: 'ECMAScript', count: 25 },
-  { value: 'Jest', count: 15 },
-  { value: 'Mocha', count: 17 },
-  { value: 'React Native', count: 27 },
-  { value: 'Angular.js', count: 30 },
-  { value: 'TypeScript', count: 15 },
-  { value: 'Flow', count: 30 },
-  { value: 'NPM', count: 11 },
-]
+interface tagCloudPros {
+    tagData : any,
+    handleTagItemClick: (tagText: string) => void;
+}
 
-export default function TagsCloud() {
+export default function TagsCloud({
+    tagData,
+    handleTagItemClick
+}: tagCloudPros) {
 
     return (
         <TagCloud
             minSize={22}
             maxSize={50}
-            tags={data}
+            tags={tagData}
             className="simple-cloud"
-            onClick={(tag : any) => alert(`'${tag.value}' was selected!`)}
+            onClick={(tag : any) => handleTagItemClick(tag.value)}
         />
     );
 }
