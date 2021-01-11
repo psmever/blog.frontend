405dca6c49d570f1cb2fa87f5e7a0c66fa051a02
psmever <psmever@gmail.com>
2021-01-11 20:17:55 +0900

    [Working] base layout.

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -8,3 +8,5 @@ export { default as PostDetailPage } from './details/PostDetailPageComponent';
 export { default as AdminLoginPage } from './admins/LoginPageComponent';
 export { default as AdminLogoutPage } from './admins/LogoutPageComponent';
 export { default as AdminWritePage } from './admins/PostWritePageComponent';
+
+export { default as TestLayoutPage } from './tests/LayoutPageComponent';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,54 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+var colors = require('tailwindcss/colors');
+
 module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'media', // or 'media' or 'class'
     theme: {
-        extend: {},
+        screens: {
+            sm: '640px',
+            // => @media (min-width: 640px) { ... }
+
+            md: '768px',
+            // => @media (min-width: 768px) { ... }
+
+            lg: '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            xl: '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
+        // padding: {
+        //     sm: '8px',
+        //     md: '16px',
+        //     lg: '24px',
+        //     xl: '48px',
+        // },
+        colors: {
+            gray: colors.coolGray,
+            blue: colors.lightBlue,
+            red: colors.rose,
+            pink: colors.fuchsia,
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            // spacing: {
+            //     128: '32rem',
+            //     144: '36rem',
+            // },
+            // borderRadius: {
+            //     '4xl': '2rem',
+            // },
+        },
     },
     variants: {
         extend: {},
     },
     plugins: [],
-}
+};
