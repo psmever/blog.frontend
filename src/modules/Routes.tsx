37797daca68127bbc698bfa0b68b0303f8329c95
psmever <psmever@gmail.com>
2021-01-14 00:24:26 +0900

    [UPDATE] navbar styles.

--- src/modules/Routes.tsx
+++ src/modules/Routes.tsx
@@ -2,12 +2,9 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
-import MainLayoutComponents from 'components/common/MainLayoutComponents';
-import * as Pages from 'components/pages';
+import { MainLayoutComponents, DefaultLayoutComponents } from '@common';
+import { DefaultPage } from '@pages';
 import useBase from 'hooks/useBase';
-// import { BodyLoading } from 'components/elements';
-
-// import { TestLayoutPage } from 'components/pages';
 
 interface RootProps {
     Routerhistory: any;
@@ -21,18 +18,20 @@ const Routes = ({ Routerhistory }: RootProps) => {
             {baseLoading === 'success' ? (
                 <ConnectedRouter history={Routerhistory}>
                     <Switch>
+                        <Route path={process.env.PUBLIC_URL + '/default'} exact={true} component={DefaultPage} />
+                        <Route path={process.env.PUBLIC_URL + '/layout'} exact={true} component={DefaultLayoutComponents} />
                         {/* <Route path={process.env.PUBLIC_URL + '/test/layout'} exact={true} component={TestLayoutPage} /> */}
                         {/* <Route path={process.env.PUBLIC_URL + '/admin/login'} exact={true} component={Pages.AdminLoginPage} />
                         <Route path={process.env.PUBLIC_URL + '/admin/logout'} exact={true} component={Pages.AdminLogoutPage} />
                         <Route path={process.env.PUBLIC_URL + '/admin/write'} exact={true} component={Pages.AdminWritePage} />
-                        <Route path={process.env.PUBLIC_URL + '/admin/:post_uuid/edit'} exact={true} component={Pages.AdminWritePage} /> */}
+                    <Route path={process.env.PUBLIC_URL + '/admin/:post_uuid/edit'} exact={true} component={Pages.AdminWritePage} /> */}
                         <MainLayoutComponents>
                             {/* <Route exact path="/" component={Pages.MainPage} />
                             <Route path={process.env.PUBLIC_URL + '/pages'} exact={true} component={Pages.MainPage} />
                             <Route path={process.env.PUBLIC_URL + '/pages/about'} exact={true} component={Pages.AboutPage} />
                             <Route path={process.env.PUBLIC_URL + '/pages/post/detail/:slug_title'} exact={true} component={Pages.PostDetailPage} />
                             <Route path={process.env.PUBLIC_URL + '/pages/tags'} exact={true} component={Pages.TagPage} />
-                            <Route path={process.env.PUBLIC_URL + '/pages/tags/:search_tag_item/search'} exact={true} component={Pages.TagPage} /> */}
+                        <Route path={process.env.PUBLIC_URL + '/pages/tags/:search_tag_item/search'} exact={true} component={Pages.TagPage} /> */}
                             {/* <Redirect path="*" to="/pages" /> */}
                         </MainLayoutComponents>
                         {/* <Route exact path={process.env.PUBLIC_URL + "/"} render={() => (<Redirect to="/pages" />)} /> */}
---
+++ src/styles/index.ts
@@ -0,0 +1,1 @@
+export { NavbarStyle } from './NavbarStyle';
