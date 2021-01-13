9178728fc6bd9dbd5f8644df639be6295c5a560e
psmever <psmever@gmail.com>
2021-01-13 23:38:27 +0900

    [UPDATE] react path aliased.

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,19 @@
+const CracoAlias = require('craco-alias');
+
 module.exports = {
     style: {
         postcss: {
-            plugins: [
-                require('tailwindcss'),
-                require('autoprefixer'),
-            ],
+            plugins: [require('tailwindcss'), require('autoprefixer')],
         },
     },
-}
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: 'tsconfig',
+                baseUrl: './src',
+                tsConfigPath: 'tsconfig.extend.json',
+            },
+        },
+    ],
+};
