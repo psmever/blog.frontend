c00a1b5f946b177f39a558031130d799064be83d
psmever <psmever@gmail.com>
2021-01-14 23:40:47 +0900

    [UPDATE] styles reset.

--- src/components/common/NavbarComponent.tsx
+++ src/components/common/NavbarComponent.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { NavbarStyle } from '@styles';
 
 const NavbarComponent = () => {
     return (
-        <nav className={NavbarStyle.NavbarNav}>
-            <div className={NavbarStyle.MainWapper}>
-                <div className={NavbarStyle.NavBox}>
-                    <div className={NavbarStyle.MobileButtonBox}>
+        <nav className="bg-gray-800">
+            <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="relative flex items-center justify-between h-16">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         {/* <!-- Mobile menu button--> */}
-                        <button className={NavbarStyle.MobileButton} aria-expanded="false">
-                            <span className={NavbarStyle.SrOly}>Open main menu</span>
+                        <button
+                            className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                            aria-expanded="false"
+                        >
+                            <span className="sr-only">Open main menu</span>
                             {/* <!-- Icon when menu is closed. --> */}
                             {/* <!--
                                 Heroicon name: menu
@@ -54,17 +56,17 @@ const NavbarComponent = () => {
                             </svg>
                         </button>
                     </div>
-                    <div className={NavbarStyle.LogoMenuBox}>
-                        <div className={NavbarStyle.LogoBox}>
+                    <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+                        <div className="flex items-center flex-shrink-0">
                             {/* <img
                                 className="block w-auto h-8 lg:hidden"
                                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                                 alt="Workflow"
                             /> */}
                             <img
-                                className={NavbarStyle.MobileLogoImage}
+                                className="block w-auto h-8 lg:hidden"
                                 src={process.env.PUBLIC_URL + `/assets/images/image_logo.png`}
-                                alt="NicePage"
+                                alt="Workflow"
                             />
                             {/* <img
                                 className="hidden w-auto h-8 lg:block"
@@ -72,41 +74,53 @@ const NavbarComponent = () => {
                                 alt="Workflow"
                             /> */}
                             <img
-                                className={NavbarStyle.LogoImage}
+                                className="hidden w-auto h-8 lg:block"
                                 src={process.env.PUBLIC_URL + `/assets/images/image_logo.png`}
-                                alt="NicePage"
+                                alt="Workflow"
                             />
                             <img
-                                className={NavbarStyle.TextLogoImage}
+                                className="hidden w-auto h-8 lg:block"
                                 src={process.env.PUBLIC_URL + `/assets/images/text_logo.png`}
-                                alt="NicePage"
+                                alt="Workflow"
                             />
 
                             {/* <p className="hidden w-auto h-8 lg:block px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">
                                 PHP - NicePage
                             </p> */}
                         </div>
-                        <div className={NavbarStyle.MenuBox}>
-                            <div className={NavbarStyle.MenuBox}>
+                        <div className="hidden sm:block sm:ml-6">
+                            <div className="flex space-x-4">
                                 {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                                <a href="#" className={NavbarStyle.ActiveTextMenu}>
-                                    Home
+                                <a
+                                    href="#"
+                                    className="px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                >
+                                    Dashboard
                                 </a>
-                                <a href="#" className={NavbarStyle.TextMenu}>
-                                    Tag
+                                <a
+                                    href="#"
+                                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+                                >
+                                    Team
                                 </a>
-                                <a href="#" className={NavbarStyle.TextMenu}>
-                                    About
+                                <a
+                                    href="#"
+                                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+                                >
+                                    Projects
                                 </a>
-                                <a href="#" className={NavbarStyle.TextMenu}>
+                                <a
+                                    href="#"
+                                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+                                >
                                     Calendar
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div className={NavbarStyle.LeftButtonBox}>
-                        <button className={NavbarStyle.NotiButton}>
-                            <span className={NavbarStyle.SrOly}>View notifications</span>
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <button className="p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                            <span className="sr-only">View notifications</span>
                             {/* <!-- Heroicon name: bell --> */}
                             <svg
                                 className="w-6 h-6"
@@ -125,15 +139,60 @@ const NavbarComponent = () => {
                             </svg>
                         </button>
 
-                        <div className={NavbarStyle.PHPButtonBox}>
-                            <button className={NavbarStyle.PHPButton} id="user-menu" aria-haspopup="true">
-                                <span className={NavbarStyle.SrOly}>Open user menu</span>
-                                <img
-                                    className="w-8 h-8 rounded-full"
-                                    src="https://media.nicepage.pe.kr/assets/blog/images/main_php.svg"
-                                    alt=""
-                                />
-                            </button>
+                        {/* <!-- Profile dropdown --> */}
+                        <div className="relative ml-3">
+                            <div>
+                                <button
+                                    className="flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                    id="user-menu"
+                                    aria-haspopup="true"
+                                >
+                                    <span className="sr-only">Open user menu</span>
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src="https://media.nicepage.pe.kr/assets/blog/images/main_php.svg"
+                                        alt=""
+                                    />
+                                </button>
+                            </div>
+                            {/* <!--
+                                Profile dropdown panel, show/hide based on dropdown state.
+
+                                Entering: "transition ease-out duration-100"
+                                From: "transform opacity-0 scale-95"
+                                To: "transform opacity-100 scale-100"
+                                Leaving: "transition ease-in duration-75"
+                                From: "transform opacity-100 scale-100"
+                                To: "transform opacity-0 scale-95"
+                            --> */}
+                            {/* <div
+                                className="absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
+                                role="menu"
+                                aria-orientation="vertical"
+                                aria-labelledby="user-menu"
+                            >
+                                <a
+                                    href="#"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                    role="menuitem"
+                                >
+                                    Your Profile
+                                </a>
+                                <a
+                                    href="#"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                    role="menuitem"
+                                >
+                                    Settings
+                                </a>
+                                <a
+                                    href="#"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                    role="menuitem"
+                                >
+                                    Sign out
+                                </a>
+                            </div> */}
                         </div>
                     </div>
                 </div>
@@ -144,19 +203,28 @@ const NavbarComponent = () => {
 
                 Menu open: "block", Menu closed: "hidden"
             --> */}
-            <div className={NavbarStyle.MobileMenuBox}>
-                <div className={NavbarStyle.MobileMenu}>
+            <div className="hidden sm:hidden">
+                <div className="px-2 pt-2 pb-3 space-y-1">
                     {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                    <a href="#" className={NavbarStyle.MobileActiveMenu}>
+                    <a href="#" className="block px-3 py-2 text-base font-medium text-white bg-gray-900 rounded-md">
                         Dashboard
                     </a>
-                    <a href="#" className={NavbarStyle.MobileNonActiveMenu}>
+                    <a
+                        href="#"
+                        className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+                    >
                         Team
                     </a>
-                    <a href="#" className={NavbarStyle.MobileNonActiveMenu}>
+                    <a
+                        href="#"
+                        className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+                    >
                         Projects
                     </a>
-                    <a href="#" className={NavbarStyle.MobileNonActiveMenu}>
+                    <a
+                        href="#"
+                        className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+                    >
                         Calendar
                     </a>
                 </div>
--- src/styles/index.ts
+++
@@ -1,1 +0,0 @@
-export { NavbarStyle } from './NavbarStyle';
