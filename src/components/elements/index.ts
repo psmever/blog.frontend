2d4a292394e62ab45f8c2f14a9a88caddd02b00c
psmever <psmever@gmail.com>
2021-01-16 00:01:42 +0900

    All Reset.

--- src/components/elements/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as MarkdownRender } from './markdown/MarkdownRender';
-export { default as MarkdownEditor } from './markdown/MarkdownEditor';
-export { default as DefaultSelectBox } from './selectbox/DefaultSelectBox';
-export { default as TagsCloud } from './tags/TagsCloud';
-export { default as WaitingPostModal } from './modal/WaitingPostModal';
--- src/template/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as TemplateDefaultPage } from './DefaultPage';
-export { default as TemplateMainPage } from './MainPage';
-
-export { default as TemplatePostPage } from './PostPage';
-export { default as TemplatePostListPage } from './PostListPage';
-export { default as TemplateAboutPage } from './AboutPage';
