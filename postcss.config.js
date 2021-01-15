cdf63ce57d692a6cff03219637c70357c78fabbf
psmever <psmever@gmail.com>
2021-01-16 00:01:42 +0900

    All Reset.

--- postcss.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    plugins: [require('tailwindcss')('./tailwind.config.js'), require('autoprefixer')],
-};
--- src/components/common/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as LeftMenu } from './LeftMenuComponent';
-export { default as RightMenu } from './RightMenuComponent';
-export { default as Navbar } from './NavbarComponent';
-export { default as MainLayoutComponents } from './MainLayoutComponents';
-export { default as DefaultLayoutComponents } from './DefaultLayoutComponents';
