325515efdb68418b0d03162a868f55ce005ab27f
psmever <psmever@gmail.com>
2021-02-02 00:00:54 +0900

    [ADD] login page publish.

--- src/Pages/Auths/LoginPage.tsx
+++ src/Pages/Auths/LoginPage.tsx
@@ -1,5 +1,73 @@
-import React from 'react';
+import React, { KeyboardEvent, useRef } from 'react';
+import { LoginWrapper, LoginMain, LoginTitle, LoginUserName, LoginPassword, LoginButton } from '@Style/LoginPageStyles';
 
 export default function LoginPage() {
-    return <>LoginPage</>;
+    const inputEmail = '';
+    const inputPassword = '';
+    const inputPasswordRef = useRef<HTMLInputElement | null>(null);
+
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key !== 'Enter') return;
+
+        const target_id: string = (e.target as HTMLElement).id;
+
+        if (target_id === 'Email') {
+            // inputPasswordRef.current?.focus();
+        }
+
+        if (target_id === 'Password') {
+            // _handleLoginButtonClick();
+        }
+    };
+
+    const _handleInputEmailChange = (value: string) => {
+        console.log(value);
+    };
+
+    const _handleInputPasswordChange = (value: string) => {
+        console.log(value);
+    };
+
+    const _handleLoginButtonClick = () => {
+        console.log('_handleLoginButtonClick');
+    };
+
+    return (
+        <>
+            <LoginWrapper>
+                <LoginMain>
+                    <LoginTitle>psmever's Blog</LoginTitle>
+
+                    <LoginUserName
+                        type="text"
+                        id="Email"
+                        value={inputEmail}
+                        onChange={e => _handleInputEmailChange(e.target.value)}
+                        placeholder="Email..."
+                        onKeyPress={e => onEnter(e)}
+                    />
+
+                    <LoginPassword
+                        type="password"
+                        id="Password"
+                        value={inputPassword}
+                        onChange={e => _handleInputPasswordChange(e.target.value)}
+                        placeholder="Password"
+                        onKeyPress={e => onEnter(e)}
+                        ref={inputPasswordRef}
+                    />
+                    <LoginButton type="button" onClick={() => _handleLoginButtonClick()}>
+                        로그인.
+                    </LoginButton>
+                    {/* {loginAttemptState === 'attempt' ? (
+                        <LoginButtonLoading />
+                    ) : (
+                        <LoginButton type="button" onClick={() => _handleLoginButtonClick()}>
+                            로그인.
+                        </LoginButton>
+                    )} */}
+                </LoginMain>
+            </LoginWrapper>
+        </>
+    );
 }
