1b11bef65ff40871571ab7bec88fc86633dc6315
psmever <psmever@gmail.com>
2021-02-03 00:39:03 +0900

    [ADD] base spinner.

---
+++ src/Styles/SpinnersStyles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const BodyLoading = styled.div`
+    margin: 4;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: '100vh';
+    display: 'flex';
+    margin-top: 50%;
+    display: block;
+    margin: 0 auto;
+    text-align: center;
+    border-color: red;
+`;
+
+export const ButtonSpinnerStyle = styled.div`
+    /* width: 100%; */
+`;
