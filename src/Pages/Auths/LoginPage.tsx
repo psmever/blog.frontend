8b268f4895483078979c9a5db7f62402f97becf6
psmever <psmever@gmail.com>
2021-02-07 09:31:24 +0900

    [Working] login page..

--- src/Pages/Auths/LoginPage.tsx
+++ src/Pages/Auths/LoginPage.tsx
@@ -1,11 +1,14 @@
 import React, { MouseEvent, KeyboardEvent, useRef, useState } from 'react';
 import { LoginWrapper, LoginMain, LoginTitle, LoginUserName, LoginPassword } from '@Style/LoginPageStyles';
 import { LoginButton } from '@Element/Buttons';
+import { loginInputState } from '@Module/InitializeConst';
+import { isValidEmail } from '@Helper';
+import _Alert_ from '@_Alert_';
+
 export default function LoginPage() {
+    const [loginInputValue, setLoginInputValue] = useState(loginInputState);
     const [loading, setLoding] = useState(false);
 
-    const inputEmail = '';
-    const inputPassword = '';
     const inputPasswordRef = useRef<HTMLInputElement | null>(null);
 
     const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
@@ -13,30 +16,49 @@ export default function LoginPage() {
 
         const target_id: string = (e.target as HTMLElement).id;
 
-        if (target_id === 'Email') {
-            // inputPasswordRef.current?.focus();
+        if (target_id === 'email') {
+            inputPasswordRef.current?.focus();
         }
 
-        if (target_id === 'Password') {
-            // _handleLoginButtonClick();
+        if (target_id === 'password') {
+            handleClickLoginButton();
         }
     };
 
-    const _handleInputEmailChange = (value: string) => {
-        console.log(value);
+    // 로그인 form 값 변경 저장 처리.
+    const handleChangeLoginInputValue = ({ name, value }: { name: string; value: string }) => {
+        setLoginInputValue({
+            ...loginInputValue,
+            [name]: value,
+        });
     };
 
-    const _handleInputPasswordChange = (value: string) => {
-        console.log(value);
-    };
+    const handleClickLoginButton = () => {
+        const checkInputValue = async ({ email, password }: { email: string; password: string }) => {
+            if (email === '' || email.length === 0) {
+                _Alert_.defaultInfo({ text: '로그인 이메일을 입력해 주세요.' });
+                return;
+            }
 
-    const _handleLoginButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
-        console.log('_handleLoginButtonClick', event);
-        if (loading === true) {
-            setLoding(false);
-        } else {
-            setLoding(true);
-        }
+            if (password === '' || password.length === 0) {
+                _Alert_.defaultInfo({ text: '로그인 비밀 번호를 입력해 주세요.' });
+                return;
+            }
+
+            if (!isValidEmail(email)) {
+                _Alert_.defaultInfo({ text: '올바른 이메일 형식을 입력해주세요.' });
+                return;
+            }
+        };
+
+        checkInputValue(loginInputValue);
+
+        // isValidEmail();
+        // if (loading === true) {
+        //     setLoding(false);
+        // } else {
+        //     setLoding(true);
+        // }
     };
 
     return (
@@ -47,23 +69,25 @@ export default function LoginPage() {
 
                     <LoginUserName
                         type="text"
-                        id="Email"
-                        value={inputEmail}
-                        onChange={e => _handleInputEmailChange(e.target.value)}
-                        placeholder="Email..."
+                        id="email"
+                        name="email"
+                        value={loginInputValue.email}
+                        onChange={e => handleChangeLoginInputValue(e.target)}
+                        placeholder="이메일 주로를 입력해주세요."
                         onKeyPress={e => onEnter(e)}
                     />
 
                     <LoginPassword
                         type="password"
-                        id="Password"
-                        value={inputPassword}
-                        onChange={e => _handleInputPasswordChange(e.target.value)}
-                        placeholder="Password"
+                        id="password"
+                        name="password"
+                        value={loginInputValue.password}
+                        onChange={e => handleChangeLoginInputValue(e.target)}
+                        placeholder="비밀 번호를 입력해 주세요."
                         onKeyPress={e => onEnter(e)}
                         ref={inputPasswordRef}
                     />
-                    <LoginButton onClick={event => _handleLoginButtonClick(event)} loading={loading} />
+                    <LoginButton onClick={() => handleClickLoginButton()} loading={loading} />
                 </LoginMain>
             </LoginWrapper>
         </>
--- src/Utils/Helper.ts
+++ src/Utils/Helper.ts
@@ -168,3 +168,8 @@ export const isEmpty = function (value: any) {
         return false;
     }
 };
+
+export const isValidEmail = (emailString: string) => {
+    const emailChack = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailChack.test(String(emailString).toLowerCase());
+};
