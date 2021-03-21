4c5e6bc77bce0ccae31074401f990404c4a751b7
psmever <psmever@gmail.com>
2021-03-21 22:08:14 +0900

    [UPDATE] helmet.

--- src/Pages/Search/SearchPage.tsx
+++ src/Pages/Search/SearchPage.tsx
@@ -1,15 +1,33 @@
 import React, { Suspense } from 'react';
+import { Helmet } from 'react-helmet';
 import { PageSpinner } from '@Element/Spinners';
 import { SearchWrapper } from '@Style/SearchPageStyles';
+import { useParams } from 'react-router-dom';
+import { isEmpty } from '@Helper';
 
 const DtlPage = React.lazy(() => import('./Dtls/SearchMainBox'));
 
 export default function SearchPage() {
+    const { search_gubun, search_str } = useParams<{
+        search_gubun: 'posts' | 'tags';
+        search_str: string;
+    }>();
+
     return (
-        <SearchWrapper>
-            <Suspense fallback={<PageSpinner />}>
-                <DtlPage />
-            </Suspense>
-        </SearchWrapper>
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>
+                    {!isEmpty(search_gubun) ? `검색` : `:: NicePage Blog ::`}{' '}
+                    {!isEmpty(search_str) ? `${search_str}` : ``}
+                </title>
+                <link rel="canonical" href="https://blog.nicepage.pe.kr/" />
+            </Helmet>
+            <SearchWrapper>
+                <Suspense fallback={<PageSpinner />}>
+                    <DtlPage />
+                </Suspense>
+            </SearchWrapper>
+        </>
     );
 }
