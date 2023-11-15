8df066818860a009d744cd78893dc61bec0185e7
psm <psm@healthmax.co.kr>
2023-11-15 16:58:42 +0900

    reset.

--- src/Store/Common/actions.ts
+++
@@ -1,9 +0,0 @@
-import { createAction } from 'typesafe-actions';
-
-export const COMMON_CODES = 'common/COMMON_CODES';
-
-export const COMMON_TAGS = 'common/COMMON_TAGS';
-export const COMMON_TAGS_SUCCESS = 'common/COMMON_TAGS_SUCCESS';
-export const COMMON_TAGS_FAILURE = 'common/CHECK_LOGIN_FAILURE';
-
-export const commonTags = createAction(COMMON_TAGS)();
