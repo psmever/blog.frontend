896387ba45f80e886cb2ccb1a6c8ad25e7da8566
psmever <psmever@gmail.com>
2023-11-21 17:14:14 +0900

    build: eslint prettier 설정 변경

--- src/app/publish/page.tsx
+++ src/app/publish/page.tsx
@@ -1,91 +1,129 @@
-import Image from "next/image";
-
-const imageLoader = () => {
-  return `https://loremflickr.com/320/240`;
-};
-
 export default function Home() {
-  return (
-    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Image
-            src="https://flowbite.com/docs/images/logo.svg"
-            width="32"
-            height="32"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
-        </a>
-        <button
-          data-collapse-toggle="navbar-solid-bg"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-solid-bg"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
+    return (
+        <div className="min-h-screen flex flex-col">
+            <header className="p-2 bg-indigo-200">Header</header>
+            <main className="flex-1 bg-indigo-50 p-2">
+                <div className="mb-20">Content</div>
+            </main>
+
+            <footer className="p-2 bg-indigo-200">Footer</footer>
         </div>
-      </div>
-    </nav>
-  );
+    );
 }
 
+/**
+
+ <div className="flex w-full">
+ <div className="">
+ <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+ <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+ <a
+ href="#"
+ className="flex items-center space-x-3 rtl:space-x-reverse"
+ >
+ <Image
+ src="https://flowbite.com/docs/images/logo.svg"
+ width="32"
+ height="32"
+ className="h-8"
+ alt="Flowbite Logo"
+ />
+ <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+ Flowbite
+ </span>
+ </a>
+ <button
+ data-collapse-toggle="navbar-solid-bg"
+ type="button"
+ className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+ aria-controls="navbar-solid-bg"
+ aria-expanded="false"
+ >
+ <span className="sr-only">Open main menu</span>
+ <svg
+ className="w-5 h-5"
+ aria-hidden="true"
+ xmlns="http://www.w3.org/2000/svg"
+ fill="none"
+ viewBox="0 0 17 14"
+ >
+ <path
+ stroke="currentColor"
+ strokeLinecap="round"
+ strokeLinejoin="round"
+ strokeWidth="2"
+ d="M1 1h15M1 7h15M1 13h15"
+ />
+ </svg>
+ </button>
+ <div
+ className="hidden w-full md:block md:w-auto"
+ id="navbar-solid-bg"
+ >
+ <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+ <li>
+ <a
+ href="#"
+ className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+ aria-current="page"
+ >
+ Home
+ </a>
+ </li>
+ <li>
+ <a
+ href="#"
+ className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+ >
+ Services
+ </a>
+ </li>
+ <li>
+ <a
+ href="#"
+ className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+ >
+ Pricing
+ </a>
+ </li>
+ <li>
+ <a
+ href="#"
+ className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+ >
+ Contact
+ </a>
+ </li>
+ </ul>
+ </div>
+ </div>
+ </nav>
+ </div>
+
+ <div className="flex flex-wrap -mb-4 -mx-2">
+ <div className="w-full sm:w-1/2 md:w-1/3 mb-4 px-2">
+ <div className="bg-indigo-400 h-12"></div>
+ </div>
+ <div className="w-full sm:w-1/2 md:w-1/3 mb-4 px-2">
+ <div className="bg-indigo-500 h-12"></div>
+ </div>
+ <div className="w-full sm:w-1/2 md:w-1/3 mb-4 px-2">
+ <div className="bg-indigo-400 h-12"></div>
+ </div>
+ <div className="w-full sm:w-1/2 md:w-1/3 mb-4 px-2">
+ <div className="bg-indigo-500 h-12"></div>
+ </div>
+ <div className="w-full sm:w-1/2 md:w-1/3 mb-4 px-2">
+ <div className="bg-indigo-400 h-12"></div>
+ </div>
+ <div className="w-full sm:w-1/2 md:w-1/3 mb-4 px-2">
+ <div className="bg-indigo-500 h-12"></div>
+ </div>
+ </div>
+ </div>
+
+
+ */
 /*
 return (
     <section className="bg-gray-100 dark:bg-gray-900 py-10 px-12">
