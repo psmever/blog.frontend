b5ff0c0944d9b59954666a2854f13754bd557826
psmever <psmever@gmail.com>
2023-11-21 13:54:30 +0900

    build: twin maco 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const withTwin = require("./withTwin");
+
 /** @type {import("next").NextConfig} */
-const nextConfig = {
+const nextConfig = withTwin({
   output: "export",
-};
+  reactStrictMode: true,
+  swcMinify: true,
+});
 
 module.exports = nextConfig;
