521a5d56d3643a900da6f2395468594a801b6320
psmever <psmever@gmail.com>
2023-12-21 17:43:57 +0900

    build: 스타일 컴포넌트 분리

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,31 +1,33 @@
-import Image from 'next/image';
+'use client';
+
 import React from 'react';
+import { PostPageStyle } from '@/Style';
+
+const { PostContainer, PostWapper, PostItemWapper, PostArticle, PostListBox, PostImageArea, PostImage, PostContentArea, PostTitle, PostContent } = PostPageStyle;
 
 export default function PostPage() {
     return (
-        <div className="mb-20">
-            <div className="max-w-screen-2xl mx-auto md:p-5">
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 gap-3">
+        <PostContainer>
+            <PostWapper>
+                <PostItemWapper>
                     {[...Array(20)].map(() => {
                         return (
-                            <div className="rounded overflow-hidden shadow-lg" key={(Math.random() + 1).toString(36).substring(7)}>
-                                <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                    <div className="flex w-full">
-                                        <Image className="object-cover rounded-lg w-full" src="https://loremflickr.com/320/240" width={320} height={240} alt="" />
-                                    </div>
+                            <PostArticle key={(Math.random() + 1).toString(36).substring(7)}>
+                                <PostListBox>
+                                    <PostImageArea>
+                                        <PostImage src="https://loremflickr.com/320/240" width={320} height={240} alt="..." />
+                                    </PostImageArea>
 
-                                    <div className="p-5">
-                                        <a href="#">
-                                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                                        </a>
-                                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                                    </div>
-                                </div>
-                            </div>
+                                    <PostContentArea>
+                                        <PostTitle>Noteworthy technology acquisitions 2021</PostTitle>
+                                        <PostContent>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</PostContent>
+                                    </PostContentArea>
+                                </PostListBox>
+                            </PostArticle>
                         );
                     })}
-                </div>
-            </div>
-        </div>
+                </PostItemWapper>
+            </PostWapper>
+        </PostContainer>
     );
 }
