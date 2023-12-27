36ddc51c026c049cadc024e29b85a4ecdaca33f7
psmever <psmever@gmail.com>
2023-12-27 10:09:14 +0900

    build: page 컴포넌트 이동, md editor test

---
+++ src/app/manage/post/editor/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { ManagePostEditorPage } from '@/PageComponent';
+
+export default function PostEditor() {
+    return <ManagePostEditorPage />;
+}
--- src/components/page/index.ts
+++ src/components/page/index.ts
@@ -1,2 +1,3 @@
 export { default as ManageLoginPage } from './manage/login-page';
 export { default as ManageLogoutPage } from './manage/logout-page';
+export { default as ManagePostEditorPage } from './manage/post-editor-page';
