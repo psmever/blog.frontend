f822f7775d02bc1fb08abaa4ab4736e6492357d4
psmever <psmever@gmail.com>
2024-01-02 11:52:11 +0900

    build: style 정리

--- src/components/page/manage/post-preview.tsx
+++ src/components/page/manage/post-preview.tsx
@@ -1,18 +1,19 @@
 import { ManagePostPageStyle } from '@/Style/common-styles';
 import { UniMarkDownView } from '@/Element';
-const { PreViewBox } = ManagePostPageStyle;
+
+const { PreViewBox, PreViewRow, PreViewTitleWapper, PreViewTitle } = ManagePostPageStyle;
 
 export default function PostPreview({ Title, Contents }: { Title: string; Contents: string }) {
     return (
         <PreViewBox>
-            <div className="flex w-full">
-                <div className="inline-block w-full h-14 min-h-14 border-b border-dotted border-gray-500">
-                    <p className="text-4xl">{Title ? Title : `제목을 입력해 주세요`}</p>
-                </div>
-            </div>
-            <div className="flex w-full">
+            <PreViewRow>
+                <PreViewTitleWapper>
+                    <PreViewTitle>{Title ? Title : `제목을 입력해 주세요`}</PreViewTitle>
+                </PreViewTitleWapper>
+            </PreViewRow>
+            <PreViewRow>
                 <UniMarkDownView Contents={Contents} />
-            </div>
+            </PreViewRow>
         </PreViewBox>
     );
 }
