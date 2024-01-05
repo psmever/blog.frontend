44fd135a652e520a5e0228fec151025f29804b3d
psmever <psmever@gmail.com>
2024-01-05 14:57:27 +0900

    build: 코드 정리

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTwin = require('./withTwin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const removeImports = require('next-remove-imports')();
 
 /** @type {import("next").NextConfig} */
