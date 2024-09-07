9f16a741349ccc6dde38849a82b2442364051ee7
psmever <psmever@gmail.com>
2024-09-08 00:08:52 +0900

    build: sample page

---
+++ src/app/(main)/publish/page.tsx
@@ -0,0 +1,9 @@
+export default function page() {
+    return (
+        <div className="flex w-full h-screen items-center justify-center">
+            <div className="flex w-full items-center justify-center">
+                <span className="text-4xl">Publish Page</span>
+            </div>
+        </div>
+    )
+}
